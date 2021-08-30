45834d0da04200e7326b21f23fb4c24f7674ffa3
cdiot <christopher.diot5@gmail.com>
2021-08-30 19:16:27 +0200

    Add registration-form

--- migrations/Version20210830122116.php
+++ migrations/Version20210830122116.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210830112411 extends AbstractMigration
+final class Version20210830122116 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Add User entity';
+        return 'Update user entity to add is_verified';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE `user`');
+        $this->addSql('ALTER TABLE user DROP is_verified');
     }
 }
