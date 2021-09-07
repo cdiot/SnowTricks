8903bb58623c32687eb9ae24754f1851ed18f500
cdiot <christopher.diot5@gmail.com>
2021-09-07 10:04:43 +0200

    Add illustration entity

---
+++ src/Entity/Illustration.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IllustrationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=IllustrationRepository::class)
+ */
+class Illustration
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Figure::class, inversedBy="illustrations")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $images;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImages(): ?Figure
+    {
+        return $this->images;
+    }
+
+    public function setImages(?Figure $images): self
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+}
